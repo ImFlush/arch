57897775ac5c1f82dd0aec031e687d460519d7b3
ImFlush <tompourie@gmail.com>
2021-03-22 22:19:03 +0100

    cleaned up functions

--- .config/fish/functions/__history_previous_argument.fish
+++ .config/fish/functions/__history_previous_argument.fish
@@ -1,7 +1,6 @@
 # !$
 function __history_previous_argument
 				set cmd (read -n 1)
-				string match -qr '^[0-9]+$' "$cmd" && set arg (read -n 1) || set arg ""
-				string match  -qr '^[0-9]+$' "$cmd$arg" && set selArg (echo $history[$cmd] | cut -d' ' -f (math $arg + 1))
-				string match -qr '^[0-9]+$' "$arg" && commandline -i " $selArg" || commandline -i "°$cmd$arg"; commandline -f repaint
+				string match -qr '^[1-9]+$' "$cmd" && set arg (read -n 1) || set arg "" && string match  -qr '^[0-9]+$' "$arg" && set selArg (echo $history[$cmd] | cut -d' ' -f (math $arg + 1)) && commandline -i " $selArg" || commandline -i "°$cmd$arg"
+				commandline -f repaint
 end
--- .config/fish/functions/__history_previous_command.fish
+++ .config/fish/functions/__history_previous_command.fish
@@ -1,9 +1,6 @@
 # bangbang
 function __history_previous_command
 				set input (read -n 1)
-				string match -qr '^[0-9]+$' "$input" && commandline -i $history[$input] && commandline -f repaint || commandline -i "!$input"; commandline -f repaint
-end
-
-function __test
-				echo "-a"
+				string match -qr '^[1-9]+$' "$input" && commandline -i $history[$input] || commandline -i "!$input"
+				commandline -f repaint
 end
